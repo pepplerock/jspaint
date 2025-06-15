ae7e9c31ca12de8dc7530d07738803c32d46048f
MarioManTAW <mariomantaw@gmail.com>
2025-06-15 09:22:18 -0500

    Update history when loading autosave

--- src/functions.js
+++ src/functions.js
@@ -973,7 +973,12 @@ function open_from_image_info(info, callback, canceled, into_existing_session, f
 	set_magnification(default_magnification);*/
 
 	if (from_session_load) {
-		createImageBitmap(info.image || info.image_data).then(function (e) { main_ctx.drawImage(e, 0, 0, e.width, e.height) });
+		createImageBitmap(info.image || info.image_data).then(function (e) {
+			main_ctx.drawImage(e, 0, 0, e.width, e.height)
+			current_history_node.name = localize("Open");
+			current_history_node.image_data = main_ctx.getImageData(0, 0, main_canvas.width, main_canvas.height);
+			current_history_node.icon = get_help_folder_icon("p_open.png");
+		});
 	}
 	else {
 		createImageBitmap(info.image || info.image_data).then(function (e) {
