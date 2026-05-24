14b06de0ae79be1fcbb8831761ffedad769ee176
pepplerock <154013925+pepplerock@users.noreply.github.com>
2026-05-24 17:48:18 -0400

    Update tool-options.js
    change smallest size to 2x2

--- src/tool-options.js
+++ src/tool-options.js
@@ -262,7 +262,7 @@ const $ChooseShapeStyle = () => {
 const $choose_brush = $Choose(
 	(() => {
 		const brush_shapes = ["circle", "square", "reverse_diagonal", "diagonal"];
-		const circular_brush_sizes = [7, 4, 1];
+		const circular_brush_sizes = [7, 4, 2];
 		const brush_sizes = [8, 5, 2];
 		const things = [];
 		brush_shapes.forEach((brush_shape) => {
